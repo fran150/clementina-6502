ca266a03d4ac03ccb6d16acf4f0eb4a853e1b006
fran150 <panchi150@gmail.com>
2025-06-25 23:34:06 -0700

    Added chip select logic components for the Clementina 6502 computer. Minor fix on making public the OR gate 74HC32

--- pkg/components/other/gates/or_74hc32.go
+++ pkg/components/other/gates/or_74hc32.go
@@ -10,15 +10,15 @@ const or74HC32NumGates int = 4
 
 // The 74CH32 chip consists of 4 OR gates. If pin a[X] and b[X] are both high
 // pin Y[X] will be low, otherwise will go high
-type or74HC32 struct {
+type Or74HC32 struct {
 	a [or74HC32NumGates]buses.LineConnector // Pin A 1 to 4 (0..3)
 	b [or74HC32NumGates]buses.LineConnector // Pin B 1 to 4 (0..3)
 	y [or74HC32NumGates]buses.LineConnector // Pin Y 1 to 4 (0..3)
 }
 
 // Creates a new 74CH32
-func New74HC32() *or74HC32 {
-	chip := or74HC32{}
+func New74HC32() *Or74HC32 {
+	chip := Or74HC32{}
 
 	for i := range or74HC32NumGates {
 		chip.a[i] = buses.NewConnectorEnabledHigh()
@@ -30,7 +30,7 @@ func New74HC32() *or74HC32 {
 }
 
 // Returns the connector for the specified pin A
-func (gate *or74HC32) APin(index int) buses.LineConnector {
+func (gate *Or74HC32) APin(index int) buses.LineConnector {
 	if index >= 0 && index < or74HC32NumGates {
 		return gate.a[index]
 	} else {
@@ -39,7 +39,7 @@ func (gate *or74HC32) APin(index int) buses.LineConnector {
 }
 
 // Returns the connector for the specified pin B
-func (gate *or74HC32) BPin(index int) buses.LineConnector {
+func (gate *Or74HC32) BPin(index int) buses.LineConnector {
 	if index >= 0 && index < or74HC32NumGates {
 		return gate.b[index]
 	} else {
@@ -48,7 +48,7 @@ func (gate *or74HC32) BPin(index int) buses.LineConnector {
 }
 
 // Returns the connector for the specified pin Y
-func (gate *or74HC32) YPin(index int) buses.LineConnector {
+func (gate *Or74HC32) YPin(index int) buses.LineConnector {
 	if index >= 0 && index < or74HC32NumGates {
 		return gate.y[index]
 	} else {
@@ -57,7 +57,7 @@ func (gate *or74HC32) YPin(index int) buses.LineConnector {
 }
 
 // Executes one emulation step
-func (gate *or74HC32) Tick(stepContext *common.StepContext) {
+func (gate *Or74HC32) Tick(stepContext *common.StepContext) {
 	for i := range or74HC32NumGates {
 		value := gate.a[i].Enabled() || gate.b[i].Enabled()
 		gate.y[i].SetEnable(value)
---
+++ pkg/computers/clementina/components/cs_logic.go
@@ -0,0 +1,134 @@
+package clementina_components
+
+import (
+	"github.com/fran150/clementina-6502/pkg/common"
+	"github.com/fran150/clementina-6502/pkg/components/buses"
+	"github.com/fran150/clementina-6502/pkg/components/other/decoders"
+	"github.com/fran150/clementina-6502/pkg/components/other/gates"
+)
+
+// This component from the Clementina 6502 computer implements the logic for the CS lines
+// It creates the following map:
+//
+// $E000 - $FFFF High RAM (32 KB banked in 8 KB windows if enabled by pico)
+// $C000 - $DFFF IO (8 slots of 1 KB each)
+// $8000 - $BFFF Extended RAM (512 KB banked in 16 KB windows)
+// $0000 - $7FFF Base RAM (32 KB)
+type ClementinaCSLogic struct {
+	// Exponsed line connectors
+	a1         [6]buses.LineConnector // A10 to A15 address lines
+	picoHiRAME buses.LineConnector    // Allows Pico to enable / disable high RAM
+
+	// Exposed lines
+	ioOE   *buses.BusConnector[uint8] // I/O output enable
+	exRAME buses.LineConnector        // External RAM enable (512 KB banked in 16 KB windows)
+	hiRAME buses.LineConnector        // High RAM enable (32 KB banked in 8 KB windows)
+
+	// Internal components
+	addressDecoder *decoders.Decoder74HC138 // Address decoder to slice RAM space in 16 KB windows
+	ioDecoder      *decoders.Decoder74HC138 // I/O decoder to slice I/O space in 8 slots of 1KB each
+	andGate        *gates.And74HC08         // Gates for logic operations
+	orGate         *gates.Or74HC32          // Gates for logic operations
+
+	addressDecoderOutput buses.Bus[uint8]
+	vcc                  buses.Line // Lines connected to VCC (will always be high)
+	ground               buses.Line // Lines connected to ground (will always be low)
+}
+
+// Creates a new Clementina CS Logic component
+func NewClementinaCSLogic() *ClementinaCSLogic {
+	csLogic := &ClementinaCSLogic{}
+
+	// Create internal components
+	csLogic.addressDecoder = decoders.NewDecoder74HC138()
+	csLogic.ioDecoder = decoders.NewDecoder74HC138()
+	csLogic.andGate = gates.New74HC08()
+	csLogic.orGate = gates.New74HC32()
+
+	csLogic.addressDecoderOutput = buses.New8BitStandaloneBus() // Output bus for the address decoder
+	csLogic.vcc = buses.NewStandaloneLine(true)                 // VCC line, always high
+	csLogic.ground = buses.NewStandaloneLine(false)             // Ground line, always low
+
+	// Expose connectors for address lines A10 to A15
+	csLogic.a1[0] = csLogic.ioDecoder.APin(0)
+	csLogic.a1[1] = csLogic.ioDecoder.APin(1)
+	csLogic.a1[2] = csLogic.ioDecoder.APin(2)
+	csLogic.a1[3] = csLogic.addressDecoder.APin(0)
+	csLogic.a1[4] = csLogic.addressDecoder.APin(1)
+	csLogic.a1[5] = csLogic.addressDecoder.EPin(2)
+
+	// Expose connectors for Pico HiRAM enable on the B pin of the OR gate
+	csLogic.picoHiRAME = csLogic.orGate.BPin(0)
+
+	// Address decoder connections
+	csLogic.addressDecoder.APin(2).Connect(csLogic.ground)
+	csLogic.addressDecoder.EPin(0).Connect(csLogic.ground)
+	csLogic.addressDecoder.EPin(1).Connect(csLogic.ground)
+	csLogic.addressDecoder.YPin().Connect(csLogic.addressDecoderOutput)
+
+	// Map gates to address decoder output
+	csLogic.andGate.APin(0).Connect(csLogic.addressDecoderOutput.GetBusLine(0))
+	csLogic.andGate.BPin(0).Connect(csLogic.addressDecoderOutput.GetBusLine(1))
+	csLogic.ioDecoder.EPin(0).Connect(csLogic.addressDecoderOutput.GetBusLine(2))
+	csLogic.ioDecoder.EPin(1).Connect(csLogic.addressDecoderOutput.GetBusLine(2))
+	csLogic.orGate.APin(0).Connect(csLogic.addressDecoderOutput.GetBusLine(3))
+
+	// Complete remaining connections on io decoder
+	csLogic.ioDecoder.EPin(2).Connect(csLogic.vcc)
+
+	// Expose output lines
+	csLogic.ioOE = csLogic.ioDecoder.YPin()
+	csLogic.exRAME = csLogic.andGate.YPin(0)
+	csLogic.hiRAME = csLogic.orGate.YPin(0)
+
+	return csLogic
+}
+
+// A1 returns the connector for the specified address line (A10 to A15)
+func (circuit *ClementinaCSLogic) A1(index int) buses.LineConnector {
+	if index >= 0 && index < len(circuit.a1) {
+		return circuit.a1[index]
+	}
+	return nil
+}
+
+// PicoHiRAME returns the connector for the Pico HiRAM enable line
+// This line is used by the Pico to enable or disable the high RAM
+// If the line is low the pico is enabling the high RAM.
+// When the clementina computer starts the Pico will respond to requests
+// in high RAM space and keep this line high disabling access to HiRAM.
+// This will be used to copy the kernel to the RAM
+// avoiding the need to use ROMs. After the pico finishes the startup
+// process it will enable the high RAM lowering this line and start
+// the execution of the Clementina 6502 kernel.
+func (circuit *ClementinaCSLogic) PicoHiRAME() buses.LineConnector {
+	return circuit.picoHiRAME
+}
+
+// IOOE returns the connector for the I/O output enable bus
+// Each line can be used to map a device in one of the 8 I/O slots
+// Each device will have 1K of available address space
+func (circuit *ClementinaCSLogic) IOOE() *buses.BusConnector[uint8] {
+	return circuit.ioOE
+}
+
+// ExRAME returns the connector for the external RAM enable line
+// The extended RAM maps to a 512 KB space banked in 16 KB windows
+func (circuit *ClementinaCSLogic) ExRAME() buses.LineConnector {
+	return circuit.exRAME
+}
+
+// HiRAME returns the connector for the high RAM enable line
+// This line is used to enable or disable the high RAM.
+// Hi RAM maps to a 32 KB space banked in 8 KB windows.
+func (circuit *ClementinaCSLogic) HiRAME() buses.LineConnector {
+	return circuit.hiRAME
+}
+
+// Tick executes one emulation step
+func (circuit *ClementinaCSLogic) Tick(stepContext *common.StepContext) {
+	circuit.addressDecoder.Tick(stepContext)
+	circuit.andGate.Tick(stepContext)
+	circuit.ioDecoder.Tick(stepContext)
+	circuit.orGate.Tick(stepContext)
+}
